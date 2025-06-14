70d0fb4004027e73a33c5e448ad6ce2ca98e7434
Andrei <An.S-1@yandex.ru>
2025-06-14 21:20:29 +0300

    ch1 commit 4

--- ch1-variationB/src/main/java/by/epam/numbers/controller/NumbersController.java
+++ ch1-variationB/src/main/java/by/epam/numbers/controller/NumbersController.java
@@ -1,14 +1,19 @@
 package by.epam.numbers.controller;
 
 import by.epam.numbers.file.NumbersFile;
+import by.epam.numbers.service.ManageServiceImpl;
 import by.epam.numbers.service.NumbersServiceImpl;
 
 import static by.epam.numbers.file.NumbersFile.loadFromFileNumbers;
 
 public class NumbersController {
-    private static final String file = "ch1-variationB\\src\\main\\java\\by\\epam\\numbers\\file";
+    private static final String file = "ch1-variationB\\src\\main\\java\\by\\epam\\numbers\\file\\NumbersFile.txt";
     private static final NumbersFile numbersFile = loadFromFileNumbers(file);
     public static NumbersServiceImpl numbersService() {
         return new NumbersServiceImpl(numbersFile);
+    }
+
+    public static ManageServiceImpl manageService() {
+        return new ManageServiceImpl(numbersFile);
     }
 }
---
+++ ch1-variationB/src/main/java/by/epam/numbers/menu/ManageMenu.java
@@ -0,0 +1,19 @@
+package by.epam.numbers.menu;
+
+public class ManageMenu extends Menu {
+
+    @Override
+    public String menu() {
+        return "";
+    }
+
+    @Override
+    public void select(String line) {
+
+    }
+
+    @Override
+    public void out() {
+
+    }
+}
