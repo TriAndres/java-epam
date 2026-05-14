424f7ce929643425f8d1dd88f71756c7928956ac
Andrei Sosnin <An.S-1@yandex.ru>
2026-05-15 01:36:18 +0300

    new 260515

--- src/main/java/ru/epam/learn/action/TutorialAction.java
+++
@@ -1,9 +0,0 @@
-package ru.epam.learn.action;
-
-public class TutorialAction {
-    public void printMessage(String msg) { // method definition
-// output string
-        System.out.println(msg);
-    }
-
-}
--- src/main/java/ru/epam/learn/arg/PrintArgumentMain.java
+++
@@ -1,9 +0,0 @@
-package ru.epam.learn.arg;
-
-public class PrintArgumentMain {
-    public static void main(String[] args){
-        for (String str : args) {
-            System.out.printf("Argument--> %s%n", str);
-        }
-    }
-}
