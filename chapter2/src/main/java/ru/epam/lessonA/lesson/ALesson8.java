1e35a3ae1c9ea50e80da9436ab000d0040575957
Andrei <An.S-1@yandex.ru>
2026-05-30 19:37:29 +0300

    260530 1

--- chapter2/src/main/java/ru/epam/lessonA/lesson/ALesson8.java
+++ chapter2/src/main/java/ru/epam/lessonA/lesson/ALesson8.java
@@ -1,13 +1,30 @@
 package ru.epam.lessonA.lesson;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ALesson8 extends ALesson {
     @Override
     public void game() {
         System.out.println("8 Среди чисел найти число-палиндром. Если таких чисел больше одного, найти второе.");
-        array = arr(0, 1000);
+        array = arr(100, 999);
         System.out.println("\nВведите количиство цифр в массиве:");
         show(array);
+
+        System.out.println("\nВывод:");
+        Arrays.sort(array);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (Integer i : array) {
+            String num1 = String.valueOf(i);
+            String num2 = new StringBuilder(num1).reverse().toString();
+            if (num1.equals(num2)) {
+                arrayList.add(i);
+            }
+        }
+        if (!arrayList.isEmpty()) {
+            System.out.println(arrayList.getLast());
+        }
 
     }
 
